383af9775698e82b967778c1eb92ad183a524796
JuHyeong424 <wkdwngud963@gmail.com>
2026-08-04 21:49:34 +0900

    refactor(fe): edit ts and tsx file becauseof eslint and prettier

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,7 @@
-import { createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import './index.css';
 
-createRoot(document.getElementById("root")!).render(<App />);
+import { createRoot } from 'react-dom/client';
+
+import App from './App';
+
+createRoot(document.getElementById('root')!).render(<App />);
