3370ed2d111f9a2965a8d1d1ef3e227315273e5c
K-iise <kimyungi1004@naver.com>
2026-08-04 15:43:02 +0900

    chore: set backend project initial

---
+++ backend/dfgg/src/test/java/dfgg/DfggApplicationTests.java
@@ -0,0 +1,13 @@
+package dfgg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DfggApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
