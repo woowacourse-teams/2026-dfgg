91f1f29b141895972df957976100bc2934f6d790
K-iise <kimyungi1004@naver.com>
2026-08-06 13:46:27 +0900

    feat: add item persistence

--- backend/dfgg/src/main/java/dfgg/infrastructure/external/dto/ItemData.java
+++ backend/dfgg/src/main/java/dfgg/infrastructure/external/dto/ItemData.java
@@ -1,10 +1,11 @@
 package dfgg.infrastructure.external.dto;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import java.util.List;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record ItemData(
-        String id,
-        String name
+        String name,
+        List<String> into
 ) {
 }
