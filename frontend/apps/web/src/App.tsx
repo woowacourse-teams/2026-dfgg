dd4b041775bb0eb928b1a1eb31da674746643cc1
JuHyeong424 <wkdwngud963@gmail.com>
2026-08-21 17:58:46 +0900

    refactor: move shared layout and footer into App

--- frontend/apps/web/src/App.tsx
+++ frontend/apps/web/src/App.tsx
@@ -1,6 +1,10 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+import SiteFooter from './components/SiteFooter';
+import TopBar from './components/TopBar';
 import ChampionSelect from './pages/champion-select/ChampionSelect';
+import DesktopApp from './pages/DesktopApp/DesktopApp';
+import Feedback from './pages/feedback/Feedback';
 import Home from './pages/home/Home';
 // import Nickname from './pages/nickname/Nickname';
 import Privacy from './pages/privacy/Privacy';
@@ -8,12 +12,23 @@ import Privacy from './pages/privacy/Privacy';
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        {/* <Route path="/nickname" element={<Nickname />} /> */}
-        <Route path="/champion-select" element={<ChampionSelect />} />
-        <Route path="/privacy" element={<Privacy />} />
-      </Routes>
+      <TopBar />
+
+      <div className='min-h-screen bg-ground'>
+        <div className='mx-auto max-w-250 px-6 pt-11 pb-13'>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            {/* <Route path="/nickname" element={<Nickname />} /> */}
+            <Route path='desktop-app' element={<DesktopApp />} />
+            <Route path='/feedback' element={<Feedback />} />
+            <Route path='/champion-select' element={<ChampionSelect />} />
+            <Route path='/privacy' element={<Privacy />} />
+          </Routes>
+        </div>
+      </div>
+      <div className='mx-auto max-w-250 px-6 pt-11 pb-13'>
+        <SiteFooter />
+      </div>
     </BrowserRouter>
   );
 }
